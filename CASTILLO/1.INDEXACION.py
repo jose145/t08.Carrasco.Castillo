a901da452cb80ab54958f786ece1fbd521dc0dc9
jose145 <56898340+jose145@users.noreply.github.com>
2019-12-11 19:57:50 -0500

    se creó el archivo 1.INDEXACION.py

---
+++ CASTILLO/1.INDEXACION.py
@@ -0,0 +1,78 @@
+#Se formaran palabras con cada letra del abecedario
+#    0         10        20
+#    012345678901234567890123456
+msg="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+#Ejercicio 16
+#Se forma Jose Castillo Jaramillo
+print(msg[9] + msg[14] + msg[18] + msg[4] + " " + msg[2] + msg[0] + msg[18] + msg[19] + msg[8] + msg[11] + msg[11] + msg[14] + " " +
+      msg[9] + msg[0] + msg[17] + msg[0] + msg[12] + msg[8] + msg[11] + msg[11] + msg[14])
+
+#Ejercicio 17
+#Se forma Colegio Nacional San Jose
+print(msg[2] + msg[14] + msg[11] + msg[4] + msg[6] + msg[8] + msg[14] + " " + msg[13] + msg[0] + msg[2] + msg[8] + msg[14] + msg[13] + msg[0] + msg[11] + " " +
+      msg[18] + msg[0] + msg[13] + " " + msg[9] + msg[14] + msg[18] + msg[4])
+
+#Ejercicio 18
+#Se forma Universitario de Deportes
+print(msg[20] + msg[13] + msg[8] + msg[21] + msg[4] + msg[17] + msg[18] + msg[8] + msg[19] + msg[0] + msg[17] + msg[8] + msg[14] +" " +
+      msg[3] + msg[4] + " " + msg[3] + msg[4] + msg[15] + msg[14] + msg[17] + msg[19] + msg[4] + msg[18])
+
+#Ejercicio 19
+#Se forma Esternocleidomastoideo
+print(msg[4] + msg[18] + msg[19] + msg[4] + msg[17] + msg[13] + msg[14] + msg[2] + msg[11] + msg[4] +  msg[8] + msg[3] + msg[14] +
+      msg[12] + msg[0] + msg[18] + msg[19] + msg[14] + msg[8] + msg[3] + msg[4] + msg[14])
+
+#Ejercicio 20
+#Se forma Facultad de ciencias fisicas y matematicas
+print(msg[5] + msg[0] + msg[2] + msg[20] + msg[11] + msg[19] + msg[0] + msg[3] + " " + msg[3] + msg[4] + " " +
+      msg[2] + msg[8] + msg[4] + msg[13] + msg[2] + msg[8] + msg[0] + msg[18] + " " + msg[5] + msg[8] + msg[18] + msg[8] + msg[2] + msg[0] + msg[18]+
+      " " + msg[24] + " " + msg[12] + msg[0] + msg[19] + msg[4] + msg[12] + msg[0] + msg[19] + msg[8] + msg[2] + msg[0] + msg[18])
+
+#Ejercicio 21
+#Se forma Feliz Aniversario
+print(msg[5] + msg[4] + msg[11] + msg[8] + msg[25] + " " +
+      msg[0] + msg[13] + msg[8] + msg[21] + msg[4] +  msg[17] + msg[18] + msg[0] + msg[17] + msg[8] + msg[14])
+
+#Ejercicio 22
+#Se forma Baby Shower
+print(msg[1] + msg[0] + msg[1] + msg[24] +  " " + msg[18] + msg[7] + msg[14] + msg[22] + msg[4] + msg[17])
+
+#Ejercicio 23
+#Se forma Felices Fiestas Patrias
+print(msg[5] + msg[4] + msg[11] + msg[8] + msg[2] + msg[4] + msg[18] + " " + msg[5] + msg[8] + msg[4] + msg[18] + msg[19] + msg[0] + msg[18] + " " +
+      msg[15] + msg[0] + msg[19] + msg[17] + msg[8] + msg[0] + msg[18])
+
+#Ejercicio 24
+#Se forma Feliz dia de la Madre
+print(msg[5] + msg[4] + msg[11] + msg[8] + msg[25] + " " + msg[3] + msg[8] + msg[0] + " " + msg[3] + msg[4] + " " + msg[11] + msg[0] + " " +
+      msg[12] + msg[0] + msg[3] + msg[17] + msg[4])
+
+#Ejercicio 25
+#Se forma Feliz dia del Padre
+print(msg[5] + msg[4] + msg[11] + msg[8] + msg[25] + " " + msg[3] + msg[8] + msg[0] + " " + msg[3] + msg[4] + msg[11]  + " " +
+      msg[15] + msg[0] + msg[3] + msg[17] + msg[4])
+
+#Ejercicio 26
+#Se forma Curso de Programacion I
+print(msg[2] + msg[20] + msg[17] + msg[18] + msg[14] + " " + msg[3] + msg[4] + " " +
+      msg[15] + msg[17] + msg[14] + msg[6] + msg[17] + msg[0] + msg[12] + msg[0] + msg[2] + msg[8] + msg[14] + msg[13] + " " + msg[8])
+
+#Ejercicio 27
+#Se forma Vamos a ver Netflix
+print(msg[20] + msg[0] + msg[12] + msg[14] + msg[18] + " " + msg[0] + " " +  msg[20] + msg[4] + msg[17] + " " +
+      msg[13] + msg[4] + msg[19] + msg[5] + msg[11] + msg[8] + msg[23])
+
+#Ejercicio 28
+#Se forma Vamos al cine
+print(msg[20] + msg[0] + msg[12] + msg[14] + msg[18] + " " + msg[0] + msg[11] + " " + msg[2] + msg[8] + msg[13] + msg[4])
+
+#Ejercicio 29
+#Se forma Nos fuimos al mundial
+print(msg[13] + msg[14] + msg[18] + " " + msg[5] + msg[20] + msg[8] + msg[12] + msg[14] + msg[18] + " " +
+      msg[0] + msg[11] + " " + msg[12] + msg[20] + msg[13] + msg[3] + msg[8] + msg[0] + msg[11])
+
+#Ejercicio 30
+#Se forma Ganamos el partido
+print(msg[6] + msg[0] + msg[13] + msg[0] + msg[12] + msg[14] + msg[18] + " " + msg[4] + msg[11] + " " +
+      msg[15] + msg[0] + msg[17] + msg[19] + msg[8] + msg[3] + msg[14])
