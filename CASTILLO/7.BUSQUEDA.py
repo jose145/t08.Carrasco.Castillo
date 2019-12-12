c5039de0fc2791f3239a850cb4388efa2e9c6934
jose145 <56898340+jose145@users.noreply.github.com>
2019-12-11 20:06:36 -0500

    se creó el archivo 7.BUSQUEDA.py

---
+++ CASTILLO/7.BUSQUEDA.py
@@ -0,0 +1,134 @@
+#Ejercicio 16
+#        0         10        20        30        40
+#        012345678901234567890123456789012345678901
+texto_1="A CABALLO REGALADO NO SE LE MIRA EL DIENTE"
+print(texto_1.find("CABALLO"))
+print(texto_1.index("MIRA"))
+print(texto_1.find("DIENTE"))
+print("#############")
+
+#Ejercicio 17
+#        0         10        20        30        40
+#        01234567890123456789012345678901234567890123456
+texto_2="AUNQUE LA MONA SE VISTA DE SEDA , MONA SE QUEDA"
+print(texto_2.find("MONA"))
+print(texto_2.index("VISTA"))
+print(texto_2.find("SEDA"))
+print("#############")
+
+#Ejercicio 18
+#        0         10        20        30
+#        0123456789012345678901234567890123456
+texto_3="EN CASA DE HERRERO , CUCHILLO DE PALO"
+print(texto_3.find("CASA"))
+print(texto_3.index("HERRERO"))
+print(texto_3.find("CUCHILLO DE PALO"))
+print("#############")
+
+#Ejercicio 19
+#        0         10        20
+#        012345678901234567890123456
+texto_4="DIOS APRIETA PERO NO AHORCA"
+print(texto_4.find("DIOS"))
+print(texto_4.index("APRIETA"))
+print(texto_4.find("NO AHORCA"))
+print("#############")
+
+#Ejercicio 20
+#        0         10        20        30
+#        01234567890123456789012345678901
+texto_5="A BUEN ENTENDEDOR POCAS PALABRAS"
+print(texto_5.find("A"))
+print(texto_5.index("BUEN"))
+print(texto_5.find("ENTENDEDOR"))
+print("#############")
+
+#Ejercicio 21
+#        0         10        20        30
+#        01234567890123456789012345678901
+texto_6="EN BOCA CERRADA NO ENTRAN MOSCAS"
+print(texto_6.find("BOCA CERRADA"))
+print(texto_6.index("NO ENTRAN"))
+print(texto_6.find("MOSCAS"))
+print("#############")
+
+#Ejercicio 22
+#        0         10        20
+#        01234567890123456789012345
+texto_7="A OTRO PERRO CON ESE HUESO"
+print(texto_7.find("A OTRO"))
+print(texto_7.index("PERRO"))
+print(texto_7.find("ESE HUESO"))
+print("#############")
+
+#Ejercicio 23
+#        0         10        20
+#        01234567890123456789012345678
+texto_8="EL QUE RIE ULTIMO , RIE MEJOR"
+print(texto_8.find("EL QUE"))
+print(texto_8.index("RIE"))
+print(texto_8.find("MEJOR"))
+print("#############")
+
+#Ejercicio 24
+#        0         10        20        30        40        50
+#        012345678901234567890123456789012345678901234567890
+texto_9="LADRON QUE ROBA A LADRON TIENE CIEN ANIOS DE PERDON"
+print(texto_9.find("LADRON"))
+print(texto_9.index("CIEN ANIOS"))
+print(texto_9.find("PERDON"))
+print("#############")
+
+#Ejercicio 25
+#         0         10        20        30
+#         0123456789012345678901234567890123
+texto_10="CON LA VARA QUE MIDES SERAS MEDIDO"
+print(texto_10.find("LA VARA"))
+print(texto_10.index("QUE MIDES"))
+print(texto_10.find("SERAS MEDIDO"))
+print("#############")
+
+#Ejercicio 26
+#         0         10        20        30        40
+#         0123456789012345678901234567890123456789012
+texto_11="MAS SABE EL DIABLO POR VIEJO QUE POR DIABLO"
+print(texto_11.find("MAS SABE"))
+print(texto_11.index("DIABLO"))
+print(texto_11.find("POR VIEJO"))
+print("#############")
+
+#Ejercicio 27
+#         0         10        20        30
+#         0123456789012345678901234567890123
+texto_12="A DIOS ROGANDO Y CON EL MAZO DANDO"
+print(texto_12.find("A DIOS"))
+print(texto_12.index("ROGANDO"))
+print(texto_12.find("MAZO DANDO"))
+print("#############")
+
+#Ejercicio 28
+#         0         10        20        30
+#         01234567890123456789012345678901
+texto_13="NO HAY MAL QUE POR BIEN NO VENGA"
+print(texto_13.find("HAY MAL"))
+print(texto_13.index("POR BIEN"))
+print(texto_13.find("NO VENGA"))
+print("#############")
+
+#Ejercicio 29
+#         0         10        20        30        40
+#         01234567890123456789012345678901234567890
+texto_14="QUIEN SIEMBRE VIENTOS COSECHA TEMPESTADES"
+print(texto_14.find("SIEMBRA"))
+print(texto_14.index("VIENTOS"))
+print(texto_14.find("TEMPESTADES"))
+print("#############")
+
+#Ejercicio 30
+#         0         10        20
+#         01234567890123456789012345678
+texto_15="A QUIEN MADRUGA DIOS LO AYUDA"
+print(texto_15.find("A QUIEN"))
+print(texto_15.index("DIOS"))
+print(texto_15.find("LO AYUDA"))
+print("#############")
