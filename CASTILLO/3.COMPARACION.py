d430c845c646a1e1fea9d57bfc9d04242eee357d
jose145 <56898340+jose145@users.noreply.github.com>
2019-12-11 20:03:58 -0500

    se creó el archivo 3.COMPARACION.py

---
+++ CASTILLO/3.COMPARACION.py
@@ -0,0 +1,119 @@
+#Ejercicio 16
+#Comparar la cadena 1 y la cadena 2
+cadena_1="PERU"
+cadena_2="BRASIL"
+#Comparador
+comparador_16=(cadena_1 == cadena_2)
+print "La cadena 1 es igual a la cadena 2?:" , comparador_16
+
+#Ejercicio 17
+#Comparar la cadena 3 y la cadena 4
+cadena_3="EL PEZ POR LA BOCA MUERE"
+cadena_4="el pez por la boca muere".upper()
+#Comparador
+comparador_17=(cadena_3 == cadena_4)
+print "La cadena 3 es igual a la cadena 4?:" , comparador_17
+
+#Ejercicio 18
+#Comparar la cadena 5 y la cadena 6
+cadena_5="MI MAMA ME MIMA"
+cadena_6="TENEMOS QUE HABLAR"
+#Comparador
+comparador_18=(cadena_5 <= cadena_6)
+print "La cadena 5 es menor o igual a la cadena 6?:" , comparador_18
+
+#Ejercicio 19
+#Comparar la cadena 7 y la cadena 8
+cadena_7="HASTA MAS TARDE"
+cadena_8="te veo luego"
+#Comparador
+comparador_19=(cadena_7 > cadena_8)
+print "La cadena 7 es mayor a la cadena 8?:" , comparador_19
+
+#Ejercicio 20
+#Comparar la cadena 9 y la cadena 10
+cadena_9="DICIEMBRE"
+cadena_10="NOVIEMBRE"
+#Comparador
+comparador_20=(cadena_9 <= cadena_10)
+print "La cadena 9 es menor o igual a la cadena 10?:" , comparador_20
+
+#Ejercicio 21
+#Comparar la cadena 11 y la cadena 12
+cadena_11="ella no te quiere"
+cadena_12="ELLA NO TE QUIERE".lower()
+#Comparador
+comparador_21=(cadena_11 == cadena_12)
+print "La cadena 11 es igual a la cadena 12?:" , comparador_21
+
+#Ejercicio 22
+#Comparar la cadena 13 y la cadena 14
+cadena_13="PROGRAMACION"
+cadena_14="aprobacion"
+#Comparador
+comparador_22=(cadena_13 >= cadena_14)
+print "La cadena 13 es mayor o igual a la cadena 14?:" , comparador_22
+
+#Ejercicio 23
+#Comparar la cadena 15 y la cadena 16
+cadena_15="hasta luego"
+cadena_16="HASTA LUEGO".lower()
+#Comparador
+comparador_23=(cadena_15 == cadena_16)
+print "La cadena 15 es igual a la cadena 16?:" , comparador_23
+
+#Ejercicio 24
+#Comparar la cadena 17 y la cadena 18
+cadena_17="feliz navidad"
+cadena_18="FELIZ NAVIDAD"
+#Comparador
+comparador_24=(cadena_17 >= cadena_18)
+print "La cadena 17 es  mayor o igual a la cadena 18?:" , comparador_24
+
+#Ejercicio 25
+#Comparar la cadena 19 y la cadena 20
+cadena_19="FELICES FIESTAS PATRIAS"
+cadena_20="felices fiestas patrias".upper()
+#Comparador
+comparador_25=(cadena_19 != cadena_20)
+print "La cadena 19 es diferente a la cadena 20?:" , comparador_25
+
+#Ejercicio 26
+#Comparar la cadena 21 y la cadena 22
+cadena_21="TE VEO MAS TARDE"
+cadena_22="te veo luego"
+#Comparador
+comparador_26=(cadena_21 <= cadena_22)
+print "La cadena 21 es menor o igual a la cadena 22?:" , comparador_26
+
+#Ejercicio 27
+#Comparar la cadena 23 y la cadena 24
+cadena_23="Felicitaciones"
+cadena_24="fELICITACIONES"
+#Comparador
+comparador_27=(cadena_23 != cadena_24)
+print "La cadena 23 es diferente a la cadena 24?:" , comparador_27
+
+#Ejercicio 28
+#Comparar la cadena 25 y la cadena 26
+cadena_25="vuelve pronto"
+cadena_26="VUELVE PRONTO".lower()
+#Comparador
+comparador_28=(cadena_25 == cadena_26)
+print "La cadena 25 es igual a la cadena 26?:" , comparador_28
+
+#Ejercicio 29
+#Comparar la cadena 27 y la cadena 28
+cadena_27="PERU"
+cadena_28="BRASIL".lower()
+#Comparador
+comparador_29=(cadena_27 <= cadena_28)
+print "La cadena 27 es menor o igual a la cadena 28?:" , comparador_29
+
+#Ejercicio 30
+#Comparar la cadena 29 y la cadena 30
+cadena_29="UNIVERSITARIO"
+cadena_30="postulante".upper()
+#Comparador
+comparador_30=(cadena_29 == cadena_30)
+print "La cadena 29 es igual a la cadena 30?:" , comparador_30
