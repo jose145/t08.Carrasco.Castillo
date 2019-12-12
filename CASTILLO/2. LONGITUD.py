25baa851ad80418a225dc8da7426a54cb2a294ab
jose145 <56898340+jose145@users.noreply.github.com>
2019-12-11 20:03:26 -0500

    se creó el archivo 2.LONGITUD.py

---
+++ CASTILLO/2. LONGITUD.py
@@ -0,0 +1,89 @@
+#Ejercicio 16
+#Calcular la lngitud de la cadena nombre
+nombre="JOSE CASTILLO JARAMILLO"
+longitud_1=len(nombre)
+print "La longitud de la cadena nombre es:" , longitud_1
+
+#Ejercicio 17
+#Calcular la lngitud de la cadena club deportivo
+club_deportivo="UNIVERSITARIO DE DEPORTES"
+longitud_2=len(club_deportivo)
+print "La longitud de la cadena club deportivo es:" , longitud_2
+
+#Ejercicio 18
+#Calcular la lngitud de la cadena libro
+libro="FUENTE OVEJUNA"
+longitud_3=len(libro)
+print "La longitud de la cadena libro es:" , longitud_3
+
+#Ejercicio 19
+#Calcular la lngitud de la cadena escuela profesional
+escuela_profesional="INGENIERIA MECANICA Y ELECTRICA"
+longitud_4=len(escuela_profesional)
+print "La longitud de la cadena escuela profesional es:" , longitud_4
+
+#Ejercicio 20
+#Calcular la lngitud de la cadena facultad
+facultad="FACULTAD DE CIENCIAS FISICAS Y MATEMATICAS"
+longitud_5=len(facultad)
+print "La longitud de la cadena facultad es:" , longitud_5
+
+#Ejercicio 21
+#Calcular la lngitud de la cadena universidad
+universidad="UNIVERSIDAD NACIONAL 'PEDRO RUIZ GALLO' "
+longitud_6=len(universidad)
+print "La longitud de la cadena universidad es:" , longitud_6
+
+#Ejercicio 22
+#Calcular la lngitud de la cadena pais
+pais="BOSNIA Y HERZEGOVINA"
+longitud_7=len(pais)
+print "La longitud de la cadena pais es:" , longitud_7
+
+#Ejercicio 23
+#Calcular la lngitud de la cadena heroe nacional
+heroe_nacional="MIGUEL GRAU SEMINARIO"
+longitud_8=len(heroe_nacional)
+print "La longitud de la cadena heroe nacional es:" , longitud_8
+
+#Ejercicio 24
+#Calcular la lngitud de la cadena colegio
+colegio="COLEGIO NACIONAL SAN JOSE"
+longitud_9=len(colegio)
+print "La longitud de la cadena colegio es:" , longitud_9
+
+#Ejercicio 25
+#Calcular la lngitud de la cadena cantante
+cantante="RICARDO MONTANER"
+longitud_10=len(cantante)
+print "La longitud de la cadena cantante es:" , longitud_10
+
+#Ejercicio 26
+#Calcular la lngitud de la cadena nombre cancion
+nombre_cancion="YO NO MEREZCO VOLVER"
+longitud_11=len(nombre_cancion)
+print "La longitud de la cadena nombre cancion es:" , longitud_11
+
+#Ejercicio 27
+#Calcular la lngitud de la cadena curso
+curso="INTRODUCCION A LA UNIVERSIDAD"
+longitud_12=len(curso)
+print "La longitud de la cadena curso es:" , longitud_12
+
+#Ejercicio 28
+#Calcular la lngitud de la cadena docente
+docente="FERNANDO DIAZ SANCHEZ"
+longitud_13=len(docente)
+print "La longitud de la cadena docente es:" , longitud_13
+
+#Ejercicio 29
+#Calcular la lngitud de la cadena presidente
+presidente="MARTIN VIZCARRA CORNEJO"
+longitud_14=len(presidente)
+print "La longitud de la cadena presidente es:" , longitud_14
+
+#Ejercicio 30
+#Calcular la lngitud de la cadena futbolista
+futbolista="PAOLO GUERRERO"
+longitud_15=len(futbolista)
+print "La longitud de la cadena futbolista es:" , longitud_15
