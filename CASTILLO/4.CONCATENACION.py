b229857e6ecd9c801e77dd32272c6904db88b488
jose145 <56898340+jose145@users.noreply.github.com>
2019-12-11 20:04:24 -0500

    se creó el archivo 4.CONCATENACION.py

---
+++ CASTILLO/4.CONCATENACION.py
@@ -0,0 +1,164 @@
+#Ejercicio 16
+#        0         10        20        30        40
+#        012345678901234567890123456789012345678901
+texto_1="A CABALLO REGALADO NO SE LE MIRA EL DIENTE"
+#CONCATENAR
+#CABELLOS DE ORO
+msg1=texto_1[2] + texto_1[3] + texto_1[4] + texto_1[33] + texto_1[34] + texto_1[34] + texto_1[8] + texto_1[22]
+msg2=texto_1[16] + texto_1[23]
+msg3=texto_1[8] + texto_1[10] + texto_1[8]
+print(msg1 + " "+  msg2 + " " + msg3)
+
+#Ejercicio 17
+#        0         10        20        30        40
+#        01234567890123456789012345678901234567890123456
+texto_2="AUNQUE LA MONA SE VISTA DE SEDA , MONA SE QUEDA"
+#CONCATENAR
+#TOQUE DE QUEDA
+msg4=texto_2[21] + texto_2[11] + texto_2[3] + texto_2[43] + texto_2[5]
+msg5=texto_2[29] + texto_2[5]
+msg6=texto_2[3] + texto_2[43] + texto_2[5] + texto_2[29] + texto_2[0]
+print(msg4 + " "+  msg5 + " " + msg6)
+
+#Ejercicio 18
+#        0         10        20        30
+#        0123456789012345678901234567890123456
+texto_3="EN CASA DE HERRERO , CUCHILLO DE PALO"
+#CONCATENAR
+#HILO DE ACERO
+msg7=texto_3[11] + texto_3[25] + texto_3[35] + texto_3[36]
+msg8=texto_3[30] + texto_3[31]
+msg9=texto_3[4] + texto_3[3] + texto_3[0] + texto_3[16] + texto_3[17]
+print(msg7 + " "+  msg8 + " " + msg9)
+
+#Ejercicio 19
+#        0         10        20
+#        012345678901234567890123456
+texto_4="DIOS APRIETA PERO NO AHORCA"
+#CONCATENAR
+#PIEDRA NO ROCA
+msg10=texto_4[6] + texto_4[8] + texto_4[9] + texto_4[0] + texto_4[7] + texto_4[11]
+msg11=texto_4[18] + texto_4[19]
+msg12=texto_4[7] + texto_4[19] + texto_4[25] + texto_4[26]
+print(msg10 + " "+  msg11 + " " + msg12)
+
+#Ejercicio 20
+#        0         10        20        30
+#        01234567890123456789012345678901
+texto_5="A BUEN ENTENDEDOR POCAS PALABRAS"
+#CONCATENAR
+#UNAS CABRAS LOCAS
+msg13=texto_5[3] + texto_5[5] + texto_5[0] + texto_5[31]
+msg14=texto_5[20] + texto_5[0] + texto_5[2] + texto_5[29] + texto_5[0] + texto_5[31]
+msg15=texto_5[26] + texto_5[15] + texto_5[20] + texto_5[0] +texto_5[31]
+print(msg13 + " "+  msg14 + " " + msg15)
+
+#Ejercicio 21
+#        0         10        20        30
+#        01234567890123456789012345678901
+texto_6="EN BOCA CERRADA NO ENTRAN MOSCAS"
+#CONCATENAR
+#CASA DE RATON
+msg16=texto_6[5] + texto_6[6] + texto_6[31] + texto_6[6]
+msg17=texto_6[13] + texto_6[19]
+msg18=texto_6[22] + texto_6[23] + texto_6[21] + texto_6[27] + texto_6[24]
+print(msg16 + " "+  msg17 + " " + msg18)
+
+#Ejercicio 22
+#        0         10        20
+#        01234567890123456789012345
+texto_7="A OTRO PERRO CON ESE HUESO"
+#CONCATENAR
+#UN HUESO ROTO
+msg19=texto_7[22] + texto_7[15]
+msg20=texto_7[21] + texto_7[22] + texto_7[23] + texto_7[24] + texto_7[25]
+msg21=texto_7[4] + texto_7[2] + texto_7[3] +texto_7[2]
+print(msg19 + " "+  msg20 + " " + msg21)
+
+#Ejercicio 23
+#        0         10        20
+#        012345678901234567890123456789
+texto_8="EL QUE RIE ULTIMO ,  RIE MEJOR"
+#CONCATENAR
+#EL MEJOR RITMO
+msg22=texto_8[0] + texto_8[1]
+msg23=texto_8[25] + texto_8[26] + texto_8[27] + texto_8[28] + texto_8[29]
+msg24=texto_8[7] + texto_8[8] + texto_8[13] +  texto_8[25] + texto_8[28]
+print(msg22 + " "+  msg23 + " " + msg24)
+
+#Ejercicio 24
+#        0         10        20        30        40        50
+#        012345678901234567890123456789012345678901234567890
+texto_9="LADRON QUE ROBA A LADRON TIENE CIEN ANIOS DE PERDON"
+#CONCATENAR
+#ANILLOS DE ORO
+msg25=texto_9[1] + texto_9[5] + texto_9[32] + texto_9[0] + texto_9[0] + texto_9[49] + texto_9[40]
+msg26=texto_9[2] + texto_9[27]
+msg27=texto_9[4] + texto_9[3] + texto_9[4]
+print(msg25 + " "+  msg26 + " " + msg27)
+
+#Ejercicio 25
+#         0         10        20        30
+#         0123456789012345678901234567890123
+texto_10="CON LA VARA QUE MIDES SERAS MEDIDO"
+#CONCATENAR
+#VERSOS DE AMOR
+msg28=texto_10[7] + texto_10[19] + texto_10[24] + texto_10[22] + texto_10[33] + texto_10[22]
+msg29=texto_10[18] + texto_10[19]
+msg30=texto_10[8] + texto_10[16] + texto_10[33] + texto_10[24]
+print(msg28 + " "+  msg29 + " " + msg30)
+
+#Ejercicio 26
+#         0         10        20        30        40
+#         0123456789012345678901234567890123456789012
+texto_11="MAS SABE EL DIABLO POR VIEJO QUE POR DIABLO"
+#CONCATENAR
+#BESO DE ODIO
+msg31=texto_11[6] + texto_11[7] + texto_11[4] + texto_11[34]
+msg32=texto_11[12] + texto_11[9]
+msg33=texto_11[17] + texto_11[12] + texto_11[13] + texto_11[27]
+print(msg31 + " "+  msg32 + " " + msg33)
+
+#Ejercicio 27
+#         0         10        20        30
+#         0123456789012345678901234567890123
+texto_12="A DIOS ROGANDO Y CON EL MAZO DANDO"
+#CONCATENAR
+#ARROZ CON LOMO
+msg34=texto_12[0] + texto_12[7] + texto_12[7] + texto_12[33] + texto_12[26]
+msg35=texto_12[17] + texto_12[18] + texto_12[19]
+msg36=texto_12[22] + texto_12[33] + texto_12[24] + texto_12[33]
+print(msg34 + " "+  msg35 + " " + msg36)
+
+#Ejercicio 28
+#         0         10        20        30
+#         01234567890123456789012345678901
+texto_13="NO HAY MAL QUE POR BIEN NO VENGA"
+#CONCATENAR
+#BUENO Y MALO
+msg37=texto_13[19] + texto_13[12] + texto_13[21] + texto_13[22] + texto_13[25]
+msg38=texto_13[5]
+msg39=texto_13[7] + texto_13[8] + texto_13[9] + texto_13[25]
+print(msg37 + " "+  msg38 + " " + msg39)
+
+#Ejercicio 29
+#         0         10        20        30        40
+#         01234567890123456789012345678901234567890
+texto_14="QUIEN SIEMBRE VIENTOS COSECHA TEMPESTADES"
+#CONCATENAR
+#QUESO DE VACA
+msg40=texto_14[0] + texto_14[1] + texto_14[3] + texto_14[40] + texto_14[19]
+msg41=texto_14[38] + texto_14[39]
+msg42=texto_14[14] + texto_14[28] + texto_14[22] + texto_14[28]
+print(msg40 + " "+  msg41 + " " + msg42)
+
+#Ejercicio 30
+#         0         10        20
+#         01234567890123456789012345678
+texto_15="A QUIEN MADRUGA DIOS LO AYUDA"
+#CONCATENAR
+#AMOR DE DIOS
+msg43=texto_15[0] + texto_15[8] + texto_15[18] + texto_15[11]
+msg44=texto_15[16] + texto_15[5]
+msg45=texto_15[16] + texto_15[17] + texto_15[18] + texto_15[19]
+print(msg43 + " "+  msg44 + " " + msg45)
