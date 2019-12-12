2947d3f36e2e9e86524e1679474f0beedd4cef97
jose145 <56898340+jose145@users.noreply.github.com>
2019-12-11 20:06:17 -0500

    se creó el archivo 6.CORTADO.py

---
+++ CASTILLO/6.CORTADO.py
@@ -0,0 +1,134 @@
+#Ejercicio 16
+#        0         10        20        30        40
+#        012345678901234567890123456789012345678901
+texto_1="A CABALLO REGALADO NO SE LE MIRA EL DIENTE"
+print texto_1[0:1] #Se forma A
+print texto_1[36:] #Se forma Diente
+print texto_1[28:32] #Se forma Mira
+print("###########################")
+
+#Ejercicio 17
+#        0         10        20        30        40
+#        01234567890123456789012345678901234567890123456
+texto_2="AUNQUE LA MONA SE VISTA DE SEDA , MONA SE QUEDA"
+print texto_2[0:6] #Se forma Aunque
+print texto_2[27:31] #Se forma Ades
+print texto_2[7:14] #Se forma La mona
+print("###########################")
+
+#Ejercicio 18
+#        0         10        20        30
+#        0123456789012345678901234567890123456
+texto_3="EN CASA DE HERRERO , CUCHILLO DE PALO"
+print texto_3[21:29] #Se forma Cuchillo
+print texto_3[33:] #Se forma Palo
+print texto_3[4:8] #Se forma Asa
+print("###########################")
+
+#Ejercicio 19
+#        0         10        20
+#        012345678901234567890123456
+texto_4="DIOS APRIETA PERO NO AHORCA"
+print texto_4[0:4] #Se forma Dios
+print texto_4[5:12] #Se forma Aprieta
+print texto_4[22:] #Se forma Horca
+print("###########################")
+
+#Ejercicio 20
+#        0         10        20        30
+#        01234567890123456789012345678901
+texto_5="A BUEN ENTENDEDOR POCAS PALABRAS"
+print texto_5[2:6] #Se forma Buen
+print texto_5[19:23] #Se forma Ocas
+print texto_5[24:28] #Se forma Pala
+print("###########################")
+
+#Ejercicio 21
+#        0         10        20        30
+#        01234567890123456789012345678901
+texto_6="EN BOCA CERRADA NO ENTRAN MOSCAS"
+print texto_6[3:7] #Se forma Boca
+print texto_6[8:13] #Se forma Cerra
+print texto_6[19:24] #Se forma Entra
+print("###########################")
+
+#Ejercicio 22
+#        0         10        20
+#        01234567890123456789012345
+texto_7="A OTRO PERRO CON ESE HUESO"
+print texto_7[7:12] #Se forma Perro
+print texto_7[17:20] #Se forma Ese
+print texto_7[2:6] #Se forma Otro
+print("###########################")
+
+#Ejercicio 23
+#        0         10        20
+#        01234567890123456789012345678
+texto_8="EL QUE RIE , ULTIMO RIE MEJOR"
+print texto_8[7:10] #Se forma Rie
+print texto_8[13:19] #Se forma Ultimo
+print texto_8[24:] #Se forma Mejor
+print("###########################")
+
+#Ejercicio 24
+#        0         10        20        30        40        50
+#        012345678901234567890123456789012345678901234567890
+texto_9="LADRON QUE ROBA A LADRON TIENE CIEN ANIOS DE PERDON"
+print texto_9[0:6] #Se forma Ladron
+print texto_9[31:35] #Se forma Cien
+print texto_9[45:] #Se forma Perdon
+print("###########################")
+
+#Ejercicio 25
+#         0         10        20        30
+#         0123456789012345678901234567890123
+texto_10="CON LA VARA QUE MIDES SERAS MEDIDO"
+print texto_10[7:11] #Se forma Vara
+print texto_10[16:21] #Se forma Mides
+print texto_10[28:] #Se forma Medido
+print("###########################")
+
+#Ejercicio 26
+#         0         10        20        30        40
+#         0123456789012345678901234567890123456789012
+texto_11="MAS SABE EL DIABLO POR VIEJO QUE POR DIABLO"
+print texto_11[12:18] #Se forma Diablo
+print texto_11[23:28] #Se forma Viejo
+print texto_11[0:8] #Se forma Mas sabe
+print("###########################")
+
+#Ejercicio 27
+#         0         10        20        30
+#         0123456789012345678901234567890123
+texto_12="A DIOS ROGANDO Y CON EL MAZO DANDO"
+print texto_12[0:6] #Se forma A Dios
+print texto_12[24:28] #Se forma Mazo
+print texto_12[7:14] #Se forma Rogando
+print("###########################")
+
+#Ejercicio 28
+#         0         10        20        30
+#         01234567890123456789012345678901
+texto_13="NO HAY MAL QUE POR BIEN NO VENGA"
+print texto_13[3:10] #Se forma Hay mal
+print texto_13[15:23] #Se forma Por bien
+print texto_13[24:] #Se forma No venga
+print("###########################")
+
+#Ejercicio 29
+#         0         10        20        30        40
+#         01234567890123456789012345678901234567890
+texto_14="QUIEN SIEMBRE VIENTOS COSECHA TEMPESTADES"
+print texto_14[6:13] #Se forma Siembre
+print texto_14[14:21] #Se forma Vientos
+print texto_14[30:] #Se forma Tempestades
+print("###########################")
+
+#Ejercicio 30
+#         0         10        20
+#         01234567890123456789012345678
+texto_15="A QUIEN MADRUGA DIOS LO AYUDA"
+print texto_15[8:15] #Se forma Madruga
+print texto_15[16:20] #Se forma Dios
+print texto_15[24:] #Se forma Ayuda
+print("###########################")
