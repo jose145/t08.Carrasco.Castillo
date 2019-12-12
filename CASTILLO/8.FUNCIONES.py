2ffd52fb74d979c383944c04a549ec760bc996ad
jose145 <56898340+jose145@users.noreply.github.com>
2019-12-11 20:06:58 -0500

    se creó el archivo 8.FUNCIONES.py

---
+++ CASTILLO/8.FUNCIONES.py
@@ -0,0 +1,137 @@
+#Ejercicio 16
+#        0         10        20        30        40
+#        012345678901234567890123456789012345678901
+texto_1="A CABALLO REGALADO NO SE LE MIRA EL DIENTE"
+print(("REGALADO").lower()) #Se muestra la cadena REGALADO en minusculas
+print(("CABALLO").lower())  #Se muestra la cadena CABALLO en minusculas
+print(("DIENTE").lower())   #Se muestra la cadena DIENTE en minusculas
+print("###################")
+
+#Ejercicio 17
+#        0         10        20        30        40
+#        01234567890123456789012345678901234567890123456
+texto_2="AUNQUE LA MONA SE VISTA DE SEDA , MONA SE QUEDA"
+print(texto_2.count("MONA")) #Se muestra cuantas vecees se repite la cadena MONA
+print(texto_2.count("SE"))   #Se muestra cuantas vecees se repite la cadena SE
+print(texto_2.count("QUEDA")) #Se muestra cuantas vecees se repite la cadena QUEDA
+print("###################")
+
+#Ejercicio 18
+#        0         10        20        30
+#        0123456789012345678901234567890123456
+texto_3="EN CASA DE HERRERO , CUCHILLO DE PALO"
+print(("en casa").upper()) #Se muestra la cadena en casa en mayusculas
+print(("herrero").upper()) #Se muestra la cadena herrero en mayusculas
+print(("cuchillo de palo").upper()) #Se muestra la cadena cuchillo de palo en mayusculas
+print("###################")
+
+#Ejercicio 19
+#        0         10        20
+#        012345678901234567890123456
+texto_4="DIOS APRIETA PERO NO AHORCA"
+print(("APRIETA").title()) #Se muestra la cadena APRIETA con la primera en mayuscula y depues en minusculas
+print(("AHORCA").title()) #Se muestra la cadena AHORCA con la primera en mayuscula y depues en minusculas
+print(("DIOS").title()) #Se muestra la cadena DIOS con la primera en mayuscula y depues en minusculas
+print("###################")
+
+#Ejercicio 20
+#        0         10        20        30
+#        01234567890123456789012345678901
+texto_5="A BUEN ENTENDEDOR POCAS PALABRAS"
+print(("BUEN").title()) #Se muestra la cadena BUEN con la primera en mayuscula y depues en minusculas
+print(("ENTENDEDOR").title()) #Se muestra la cadena ENTENDEDOR con la primera en mayuscula y depues en minusculas
+print(("POCAS PALABRAS").title()) #Se muestra la cadena POCAS PALABRAS con la primera en mayuscula y depues en minusculas
+print("###################")
+
+#Ejercicio 21
+#        0         10        20        30
+#        01234567890123456789012345678901
+texto_6="EN BOCA CERRADA NO ENTRAN MOSCAS"
+print(("en boca").upper()) #Se muestra la cadena en boca en mayusculas
+print(("cerrada").upper()) #Se muestra la cadena cerrada en mayusculas
+print(("entran moscas").upper()) #Se muestra la cadena entran moscas en mayusculas
+print("###################")
+
+#Ejercicio 22
+#        0         10        20
+#        01234567890123456789012345
+texto_7="A OTRO PERRO CON ESE HUESO"
+print(("OTRO").lower()) #Se muestra la cadena OTRO en minusculas
+print(("PERROO").lower())  #Se muestra la cadena PERRO en minusculas
+print(("ESE HUESO").lower())   #Se muestra la cadena ESE HUESO en minusculas
+print("###################")
+
+#Ejercicio 23
+#        0         10        20
+#        012345678901234567890123456789
+texto_8="EL QUE RIE ULTIMO ,  RIE MEJOR"
+print(texto_2.count("RIE")) #Se muestra cuantas vecees se repite la cadena RIE
+print(texto_2.count("EL"))   #Se muestra cuantas vecees se repite la cadena EL
+print(texto_2.count("MEJOR")) #Se muestra cuantas vecees se repite la cadena MEJOR
+print("###################")
+
+
+#Ejercicio 24
+#        0         10        20        30        40        50
+#        012345678901234567890123456789012345678901234567890
+texto_9="LADRON QUE ROBA A LADRON TIENE CIEN ANIOS DE PERDON"
+print(("ladron").upper()) #Se muestra la cadena ladron en mayusculas
+print(("que roba").upper()) #Se muestra la cadena que roba en mayusculas
+print(("anios de perdon").upper()) #Se muestra la cadena anios de perdon en mayusculas
+print("###################")
+
+
+#Ejercicio 25
+#         0         10        20        30
+#         0123456789012345678901234567890123
+texto_10="CON LA VARA QUE MIDES SERAS MEDIDO"
+print(texto_2.count("CON")) #Se muestra cuantas vecees se repite la cadena CON
+print(texto_2.count("LA"))   #Se muestra cuantas vecees se repite la cadena LA
+print(texto_2.count("VARA")) #Se muestra cuantas vecees se repite la cadena VARA
+print("###################")
+
+#Ejercicio 26
+#         0         10        20        30        40
+#         0123456789012345678901234567890123456789012
+texto_11="MAS SABE EL DIABLO POR VIEJO QUE POR DIABLO"
+print(("SABE").lower()) #Se muestra la cadena SABE en minusculas
+print(("VIEJO").lower())  #Se muestra la cadena VIEJO en minusculas
+print(("POR DIABLO").lower())   #Se muestra la cadena POR DIABLO en minusculas
+print("###################")
+
+#Ejercicio 27
+#         0         10        20        30
+#         0123456789012345678901234567890123
+texto_12="A DIOS ROGANDO Y CON EL MAZO DANDO"
+print(("A DIOS").title()) #Se muestra la cadena A DIOS con la primera en mayuscula y depues en minusculas
+print(("ROGANDO").title()) #Se muestra la cadena ROGANDO con la primera en mayuscula y depues en minusculas
+print(("MAZO DANDO").title()) #Se muestra la cadena MAZO DANDO con la primera en mayuscula y depues en minusculas
+print("###################")
+
+#Ejercicio 28
+#         0         10        20        30
+#         01234567890123456789012345678901
+texto_13="NO HAY MAL QUE POR BIEN NO VENGA"
+print(("HAY MAL").lower()) #Se muestra la cadena HAY MAL en minusculas
+print(("BIEN").lower())  #Se muestra la cadena BIEN en minusculas
+print(("NO VENGA").lower())   #Se muestra la cadena NO VENGA en minusculas
+print("###################")
+
+#Ejercicio 29
+#         0         10        20        30        40
+#         01234567890123456789012345678901234567890
+texto_14="QUIEN SIEMBRE VIENTOS COSECHA TEMPESTADES"
+print(("A QUIEN").title()) #Se muestra la cadena AQUIEN con la primera en mayuscula y depues en minusculas
+print(("SIEMBRA VIENTOS").title()) #Se muestra la cadena SIEMBRA VIENTOS con la primera en mayuscula y depues en minusculas
+print(("TEMPESATADES").title()) #Se muestra la cadena TEMPESTADES con la primera en mayuscula y depues en minusculas
+print("###################")
+
+#Ejercicio 30
+#         0         10        20
+#         01234567890123456789012345678
+texto_15="A QUIEN MADRUGA DIOS LO AYUDA"
+print(texto_2.count("QUIEN")) #Se muestra cuantas vecees se repite la cadena QUIEN
+print(texto_2.count("A"))   #Se muestra cuantas vecees se repite la cadena A
+print(texto_2.count("DIOS")) #Se muestra cuantas vecees se repite la cadena DIOS
+print("###################")
+
