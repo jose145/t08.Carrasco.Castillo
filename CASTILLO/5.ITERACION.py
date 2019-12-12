006ed4fa0263f4a2ce4a34fa10085efc01d7dc9a
jose145 <56898340+jose145@users.noreply.github.com>
2019-12-11 20:05:55 -0500

    se creó el archivo 5.ITERACION.py

---
+++ CASTILLO/5.ITERACION.py
@@ -0,0 +1,163 @@
+#Ejercicio 16
+#        0         10        20        30        40
+#        012345678901234567890123456789012345678901
+texto_1="A CABALLO REGALADO NO SE LE MIRA EL DIENTE"# Iteracion
+for item in texto_1.split():
+    print (item)
+#fin_iteracion
+
+print("#############################")
+
+#Ejercicio 17
+#        0         10        20        30        40
+#        01234567890123456789012345678901234567890123456
+texto_2="AUNQUE LA MONA SE VISTA DE SEDA , MONA SE QUEDA"
+# Iteracion
+for item in texto_2.split():
+    print (item)
+#fin_iteracion
+
+print("#############################")
+
+#Ejercicio 18
+#        0         10        20        30
+#        0123456789012345678901234567890123456
+texto_3="EN CASA DE HERRERO , CUCHILLO DE PALO"
+# Iteracion
+for item in texto_3.split():
+    print (item)
+#fin_iteracion
+
+print("#############################")
+
+#Ejercicio 19
+#        0         10        20
+#        012345678901234567890123456
+texto_4="DIOS APRIETA PERO NO AHORCA"
+# Iteracion
+for item in texto_4.split():
+    print (item)
+#fin_iteracion
+
+print("#############################")
+
+#Ejercicio 20
+#        0         10        20        30
+#        01234567890123456789012345678901
+texto_5="A BUEN ENTENDEDOR POCAS PALABRAS"
+# Iteracion
+for item in texto_5.split():
+    print (item)
+#fin_iteracion
+
+print("#############################")
+
+#Ejercicio 21
+#        0         10        20        30
+#        01234567890123456789012345678901
+texto_6="EN BOCA CERRADA NO ENTRAN MOSCAS"
+# Iteracion
+for item in texto_6.split():
+    print (item)
+#fin_iteracion
+
+print("#############################")
+
+#Ejercicio 22
+#        0         10        20
+#        01234567890123456789012345
+texto_7="A OTRO PERRO CON ESE HUESO"
+# Iteracion
+for item in texto_7.split():
+    print (item)
+#fin_iteracion
+
+print("#############################")
+
+#Ejercicio 23
+#        0         10        20
+#        012345678901234567890123456789
+texto_8="EL QUE RIE ULTIMO ,  RIE MEJOR"
+# Iteracion
+for item in texto_8.split():
+    print (item)
+#fin_iteracion
+
+print("#############################")
+
+#Ejercicio 24
+#        0         10        20        30        40        50
+#        012345678901234567890123456789012345678901234567890
+texto_9="LADRON QUE ROBA A LADRON TIENE CIEN ANIOS DE PERDON"
+# Iteracion
+for item in texto_9.split():
+    print (item)
+#fin_iteracion
+
+print("#############################")
+
+#Ejercicio 25
+#         0         10        20        30
+#         0123456789012345678901234567890123
+texto_10="CON LA VARA QUE MIDES SERAS MEDIDO"
+# Iteracion
+for item in texto_10.split():
+    print (item)
+#fin_iteracion
+
+print("#############################")
+
+#Ejercicio 26
+#         0         10        20        30        40
+#         0123456789012345678901234567890123456789012
+texto_11="MAS SABE EL DIABLO POR VIEJO QUE POR DIABLO"
+# Iteracion
+for item in texto_11.split():
+    print (item)
+#fin_iteracion
+
+print("#############################")
+
+#Ejercicio 27
+#         0         10        20        30
+#         0123456789012345678901234567890123
+texto_12="A DIOS ROGANDO Y CON EL MAZO DANDO"
+# Iteracion
+for item in texto_12.split():
+    print (item)
+#fin_iteracion
+
+print("#############################")
+
+#Ejercicio 28
+#         0         10        20        30
+#         01234567890123456789012345678901
+texto_13="NO HAY MAL QUE POR BIEN NO VENGA"
+# Iteracion
+for item in texto_13.split():
+    print (item)
+#fin_iteracion
+
+print("#############################")
+
+#Ejercicio 29
+#         0         10        20        30        40
+#         01234567890123456789012345678901234567890
+texto_14="QUIEN SIEMBRE VIENTOS COSECHA TEMPESTADES"
+# Iteracion
+for item in texto_14.split():
+    print (item)
+#fin_iteracion
+
+print("#############################")
+
+#Ejercicio 30
+#         0         10        20
+#         01234567890123456789012345678
+texto_15="A QUIEN MADRUGA DIOS LO AYUDA"
+# Iteracion
+for item in texto_15.split():
+    print (item)
+#fin_iteracion
+
+print("#############################")
